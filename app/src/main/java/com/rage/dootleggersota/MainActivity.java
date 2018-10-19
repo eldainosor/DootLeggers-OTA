dac6dfd15cdb271ba6f38cd48ae51582d69d3d69
Hardik <srivastavahardik@gmail.com>
2018-10-19 12:15:48 +0530

    Created Fragment for the base (No updates Available)  Layout

--- app/src/main/java/com/rage/dootleggersota/MainActivity.java
+++ app/src/main/java/com/rage/dootleggersota/MainActivity.java
@@ -2,12 +2,20 @@ package com.rage.dootleggersota;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+import com.rage.dootleggersota.Fragments.BaseFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int container = R.id.frameLayoutContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BaseFragment baseFragment = new BaseFragment();
+        getSupportFragmentManager().beginTransaction().replace(container, baseFragment).commit();
     }
 }
