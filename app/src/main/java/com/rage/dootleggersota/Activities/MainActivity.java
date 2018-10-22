5390b85101ea2ad91fde85b1be6fc70ff61aebd9
Hardik <srivastavahardik@gmail.com>
2018-10-22 21:24:45 +0530

    Added function to  Refresh Button

--- app/src/main/java/com/rage/dootleggersota/Activities/MainActivity.java
+++ app/src/main/java/com/rage/dootleggersota/Activities/MainActivity.java
@@ -1,9 +1,13 @@
 package com.rage.dootleggersota.Activities;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.rage.dootleggersota.Fragments.BaseFragment;
 import com.rage.dootleggersota.Fragments.UnableFragment;
@@ -15,12 +19,28 @@ import com.rage.dootleggersota.Utils.ExecShell;
 public class MainActivity extends AppCompatActivity {
 
     private int container = R.id.frameLayoutContainer;
+    private View refresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkUpdate();
+        defineLayout();
+    }
 
+    private void defineLayout () {
+        refresh = findViewById(R.id.viewCheckUpdate);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Checking..", Toast.LENGTH_SHORT).show();
+                checkUpdate();
+            }
+        });
+    }
+
+    private void checkUpdate () {
         CheckUpdate checkUpdate = new CheckUpdate();
         if (checkUpdate.isAbleToCheckUpdate()) {
             if (checkUpdate.isUpdateAvailable()) {
